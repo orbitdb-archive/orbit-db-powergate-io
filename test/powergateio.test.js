3c564a483390a27d009a3b60a074939a18cc5379
Mark Henderson <mark@mrh.io>
2020-09-23 01:32:00 -0400

    quick additional test comments

--- test/powergateio.test.js
+++ test/powergateio.test.js
@@ -114,24 +114,32 @@ Object.keys(testAPIs).forEach(API => {
       })
 
       it('stores a snapshot and creates an entry in the jobs db', async () => {
+        // Submit a db address, get back a receipt for a storge job. The
+        // job status will be executing. This won't complete until the db has replicated
+        // See: https://github.com/textileio/powergate/blob/master/ffs/rpc/rpc.proto#L103
         jobStatus = await powergateio.storeSnapshot(db.address.toString())
 
         assert.strictEqual(jobStatus.dbAddress, db.address.toString())
+        // See: https://github.com/textileio/powergate/blob/master/ffs/rpc/rpc.proto#L103
         assert.strictEqual(jobStatus.status, JobStatus.JOB_STATUS_EXECUTING)
         assert.strictEqual(jobStatus.errCause, '')
         assert.strictEqual(jobStatus.dealErrorsList.length, 0)
 
+        // Drop db after snapshotting it
         await db.drop()
       })
 
       it('reports the job status immediately', async () => {
+        // Queries the internal database by job ID
         const reportedStatus = await powergateio.getJobStatus(jobStatus.id)
 
         assert.strictEqual(jobStatus.cid, reportedStatus[0].cid)
+        // See: https://github.com/textileio/powergate/blob/master/ffs/rpc/rpc.proto#L103
         assert.strictEqual(reportedStatus[0].status, JobStatus.JOB_STATUS_EXECUTING)
       })
 
       it('retrieves a remote database snapshot from "hot" storage by CID', async () => {
+        // Open a brand new empty database (original db is dropped above)
         db2 = await orbitdb.open(db.address.toString(), { create: true })
 
         assert.strictEqual(db.address.toString(), db2.address.toString())
@@ -140,10 +148,13 @@ Object.keys(testAPIs).forEach(API => {
         await db2.load()
         assert.strictEqual(db2._oplog.values.length, 0)
 
+        // Retrieve an array of snapshots from powergateio
+        // Right now we're only testing one, TODO test for multiple
         snapshots = await powergateio.retrieveSnapshot(db2.address.toString())
 
         assert.strictEqual(snapshots.length, 1)
         assert.strictEqual(snapshots[0].job.dbAddress, db.address.toString())
+        // See: https://github.com/textileio/powergate/blob/master/ffs/rpc/rpc.proto#L103
         assert.strictEqual(snapshots[0].job.status, JobStatus.JOB_STATUS_EXECUTING)
         assert.strictEqual(snapshots[0].job.errCause, '')
         assert.strictEqual(snapshots[0].job.dealErrorsList.length, 0)
@@ -153,6 +164,8 @@ Object.keys(testAPIs).forEach(API => {
       })
 
       it('loads the snapshot into a fully-functioning OrbitDB database', async () => {
+        // FIXME: These steps are necessary but should be abstracted into a
+        // public UI somehow, or hidden.
         await db2._oplog.join(snapshots[0].log)
         await db2._updateIndex()
 
@@ -161,12 +174,16 @@ Object.keys(testAPIs).forEach(API => {
         }
       })
 
+      // The tests continue for local-only tests, since these could take
+      // up to 20 hours on testnet. So we skip them there for now.
       if (IS_LOCAL) {
         this.timeout(240000)
 
         it('eventually reports the job as completed', (done) => {
+          // Ping getJobStatus every 2 seconds until status reports success
           const interval = setInterval(async () => {
             const status = await powergateio.getJobStatus(jobStatus.id)
+            // See: https://github.com/textileio/powergate/blob/master/ffs/rpc/rpc.proto#L103
             if (status[0].status === JobStatus.JOB_STATUS_SUCCESS) {
               assert(true)
               clearInterval(interval)
@@ -176,11 +193,15 @@ Object.keys(testAPIs).forEach(API => {
         })
 
         it('does not pollute the db via watchJobs', async () => {
+          // Here, we're testing that the instantaneous status reported
+          // from PowergateIO after the job completes matches the original
+          // JobStatus we got back on job creation
           const status = await powergateio.getJobStatus(jobStatus.id)
 
           assert.strictEqual(status[0].id, jobStatus.id)
           assert.strictEqual(status[0].apiId, jobStatus.apiId)
           assert.strictEqual(status[0].cid, jobStatus.cid)
+          // See: https://github.com/textileio/powergate/blob/master/ffs/rpc/rpc.proto#L103
           assert.strictEqual(status[0].status, JobStatus.JOB_STATUS_SUCCESS)
           assert.strictEqual(status[0].dbAddress, jobStatus.dbAddress)
         })
